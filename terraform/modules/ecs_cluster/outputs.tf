7dc9f9b34262231a1131752d43dc190b12594d43
CloudNexus <abhighnagiri@gmail.com>
2026-02-03 13:01:18 +0530

    Add outputs for ECS cluster and services

---
+++ terraform/modules/ecs_cluster/outputs.tf
@@ -0,0 +1,11 @@
+output "cluster_name"  {
+    value =  aws_ecs_cluster.this.name
+}
+
+output "service_name"  {
+    value =  aws_ecs_service.app.name
+}
+
+output "alb_dns_name"  {
+    value =  aws_lb.app.dns_name
+}
