3d72b72ada2eec9f96f8214276024ffa32a16e8c
CloudNexus <abhighnagiri@gmail.com>
2026-02-03 13:02:47 +0530

    Add environment and region variables to Terraform

---
+++ terraform/networking/variables.tf
@@ -0,0 +1,9 @@
+variable  "env"  {
+   type               =  string
+   description  =  "Environment name  (e.g.,  dev,  prod)"
+}
+
+variable  "region"  {
+   type               = string
+    default         =  "eu-central-1"
+}
